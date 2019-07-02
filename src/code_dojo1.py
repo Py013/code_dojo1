1a4ceac031495e89015f44814b242b685b74be8c
Elder <elder_louzada@outlook.com>
2019-07-02 10:07:24 -0300

    função fatores_primos atualizada

--- src/code_dojo1.py
+++ src/code_dojo1.py
@@ -24,39 +24,61 @@ def ano_bissexto(ano):
 Desafio2: fatores primos
 Enunciado em: http://dojopuzzles.com/problemas/exibe/geracao-de-fatores-primos/
 '''
-def verifica_primos(numero):
-    count = 0
-    for x in range(1, numero+1):
-        if (numero % (x) == 0):
-            count += 1
-        if count > 2:
-            return False
-
-    return True
 
 
+def verifica_primos(num):
+    count = 0 
+    for i in range(1, num+1):  
+        if (num % i) == 0:  
+            count += 1  
+    return count == 2  
 
-def fatores_primos(numero):
-    resultado = numero
-    fatores = []
-    #if verifica_primos(numero):
-        #resultado = numero
-        #fatores = []
-    'for x in range(numero, reverse = true): Possível aplicabilidade mas eu n soube utilizar o reverse'
-    #while not verifica_primos(resultado):
-    for x in range(2, numero):
-        if (resultado % x == 0)):
+# 
+def fatores_primos(num):
+    fatores = []  
+    i = 2 
+    if int(num) <= 1 or type(num) is str: 
+        return fatores
+    while i < num+1: 
+        if verifica_primos(i) and num % i == 0:  
+            fatores.append(i)  
+            num = num/i 
+            i = 2 
+            continue 
+        i += 1 
+    return fatores 
 
-        resultado = int(resultado / x)
-        fatores.append(x)
+#
+# def testa_programa():
+#     num = int(input("Digite o numero a ser fatorado: "))
+#     print(fatores_primos(num))
+#     pass
 
+# testa_programa()
+'''
+verifica_primos(num)
+linha | Explicação
+1       Função que verifica se um numero eh primo, retornando um boolean
+2       Cria um contador
+3       Faz um looping de intervalo [1,num+1]
+4       Esse If foi feito para averiguar a quantidade de divisores que 'num' tem. Para ser primo 'num' deve ter apenas dois divisores - 1 e 'num' - caso contrário não será classificado dessa forma
+5       count irá incrementar de acordo com a quantidade de divisores que num possuir
+7       Se count for igual a dois significa que 'num' é primo, pois só é divisível por um e ele mesmo, logo a função retorna True, caso contrário retornará False
+fatores_primos(num)
+1       Função criada para verificar todos os fatores primos que formam um número 'num'
+2       Criamos uma lista vazia, esta armazenará os fatores primos
+3       Essa variável será utilizada no looping, começa em dois pois esse é o menor número primo disponível.
+4       verifica se o número passado é menor ou igual a 1, ou se ele é na verdade uma String. Nesses casos, retorna-se uma lista vazia uma vez que não é possível fatorar      
+5       A laço while é criado para realizar o looping no intervalo de [i, num+1]
+6       Verifica se o numero da vez é primo e divisor de 'num', nesse caso ele também será um fator de 'num'
+7       Por ser fator de 'num', 'i' é incluído na lista criada anteriormente 
+8       O valor de num é atualizado, sendo dividido por seu fator primo encontrado para que possa ser fatorado novamente na próxima iteração
+9       O valor de 'i' volta a ser 2 porque cada fatoração precisa ser feita com o menor numero primo que também é divisor do número a ser fatorado
+10      Nesse caso, seguimos para o próximo looping uma vez que conseguimos encontrar um dos fatores primos de 'num'
+11      Caso não for possível encontrar um dos fatores primos de num nessa iteração, incrementamos 'i' e seguimos para a próxima
+12      Retornamos a lista de fatores ao final do processo
+'''
 
-
-    #if count > 2:
-    #    return False
-
-
-    return fatores
 
 '''
 Desafio3: Jokenpo
